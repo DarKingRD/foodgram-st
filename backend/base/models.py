7d4176e1da067db030277e9be754d7cda265a818
Денис Баранов <finedend@gmail.com>
2025-01-25 02:48:22 +0300

    Created models

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,154 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.utils.timezone import now
+
+# Получаем модель пользователя
+User = get_user_model()
+
+
+# Модель игредиента
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=128,
+        verbose_name='Название')
+    measurement_unit = models.CharField(
+        max_length=64,
+        verbose_name='Ед. измерения')
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name} ({self.measurement_unit})'
+
+
+# Модель рецепта
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор')
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название')
+    image = models.ImageField(
+        upload_to='recipes/images',
+        verbose_name='Изображение')
+    text = models.TextField(verbose_name='Описание')
+    cooking_time = models.PositiveIntegerField(
+        verbose_name='Время приготовления (мин)')
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ['-date_published']
+
+    def __str__(self):
+        return self.name
+
+
+# Модель избранное
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь')
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт')
+
+    class Meta:
+        unique_together = ('user', 'recipe')
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
+
+    def __str__(self):
+        return f'{self.user.username} добавил в избранное {self.recipe.name}'
+
+
+# Модель подписки
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор')
+    recipes_count = models.IntegerField(verbose_name='Кол-во рецептов')
+
+    class Meta:
+        unique_together = ['user', 'author']
+        ordering = ['id']
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f'{self.user.username} подписан на {self.author.username}'
+
+
+# Модель корзины
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт')
+
+    class Meta:
+        unique_together = ('user', 'recipe')
+        verbose_name = 'Корзина покупок'
+        verbose_name_plural = 'Корзины покупок'
+
+    def __str__(self):
+        return f'У {self.user.username} есть {self.recipe.name} в корзине'
+
+
+# Модель для связи рецепта и ингредиента
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт')
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент')
+    amount = models.IntegerField(verbose_name='Количество')
+
+    class Meta:
+        unique_together = ('recipe', 'ingredient')
+        verbose_name = 'Ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецептов'
+
+    def __str__(self):
+        return f'{self.amount} {self.ingredient} в {self.recipe.name}'
+
+
+# Модель профиля
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь')
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        blank=True,
+        null=True,
+        verbose_name='Аватарка')
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+    def __str__(self):
+        return f'Профиль {self.user.username}'
